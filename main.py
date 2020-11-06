3bac07a06896f5be393a7ab7665e1672847ab6b0
Anastasia Hudyma <anastasiia.hudyma.knm.2019@lpnu.ua>
2020-11-06 20:00:46 +0200

    master form

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from gevent.pywsgi import WSGIServer
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/api/v1/hello-world-5')
+@app.route('/api/v1/hello-world')
 def hello_world():
-    return 'Hello World 5'
+    return 'Hello World'
 
 server = WSGIServer(('127.0.0.1',5000),app)
 server.serve_forever()
