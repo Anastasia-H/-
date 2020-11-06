ad7d2332794a9fc3fdecf151c6bc93730d2da031
Anastasia Hudyma <anastasiia.hudyma.knm.2019@lpnu.ua>
2020-11-06 19:57:20 +0200

    another change

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from gevent.pywsgi import WSGIServer
+from flask import Flask
+app = Flask(__name__)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@app.route('/api/v1/hello-world')
+def hello_world():
+    return 'Hello World'
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+server = WSGIServer(('127.0.0.1',5000),app)
+server.serve_forever()
